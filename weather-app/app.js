965056be9be8427a3bf65f352b6dbf5a4d515b89
07shikhar <shikhar.sharma0793@gmail.com>
2021-11-17 12:41:04 +0530

    Basics

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=43177dbf4b6067a4a2ae4fb41bfcdfe6&query=37.8267,-122.4233'
+request({url: url}, (error, response)=>{ //response is string representation of JSON
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
