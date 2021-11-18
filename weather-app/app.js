c6a1537c818d81f44b48a12f2a26943a2579a007
07shikhar <shikhar.sharma0793@gmail.com>
2021-11-18 08:32:09 +0530

    Auto-parsing

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,11 @@
 const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=43177dbf4b6067a4a2ae4fb41bfcdfe6&query=37.8267,-122.4233'
-request({url: url}, (error, response)=>{ //response is string representation of JSON
-    const data = JSON.parse(response.body)
-    console.log(data.current)
-})
+// request({url: url}, (error, response)=>{ //response is string representation of JSON
+//     const data = JSON.parse(response.body)
+//     console.log(data.current)
+// }) manal parsing of JSON data received by API
+
+request({url: url, json: true}, (error, response)=>{
+    console.log(response.body.current)
+}) // auto-parsing
