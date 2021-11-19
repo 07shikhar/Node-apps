4805a072cdf0e7786a2fd2d1948d91752acdf53c
07shikhar <shikhar.sharma0793@gmail.com>
2021-11-19 11:30:17 +0530

    App

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,32 +1,11 @@
-// const request = require('request')
-// const geocode = require('./utils/geocode')
+const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-// const url = 'http://api.weatherstack.com/current?access_key=43177dbf4b6067a4a2ae4fb41bfcdfe6&query=37.8267,-122.4233'
-// // request({url: url}, (error, response)=>{ //response is string representation of JSON
-// //     const data = JSON.parse(response.body)
-// //     console.log(data.current)
-// // }) manual parsing of JSON data received by API
+geocode('Mumbai', (error, data)=>{
+    console.log('Error', error)
+    console.log('Data', data)
 
-// request({url: url, json: true}, (error, response)=>{
-//     if (error){
-//         console.log("Unable to connect to the API")
-//     }
-//     else if(response.body.error){
-//         console.log("Unable to find Location")
-//     }
-//     else{
-//         const rBdy = response.body.current
-//         console.log("The temperatue is " + rBdy.temperature + " and there are " + rBdy.feelslike + "% chances of rain.")
-
-//     }
-    
-// }) // auto-parsing
-// geocode('Mumbai', (error, data)=>{
-//     console.log('Error', error)
-//     console.log('Data', data)
-
-// })
+})
 forecast(18.96667,72.83333,(error,data)=>{
     console.log('Error', error)
     console.log('Data', data)
